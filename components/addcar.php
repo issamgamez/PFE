1810528de7d5afdce457de00593412634ff69d45
YassineRazkaoui <razkaouiyassine9@gmail.com>
2024-06-08 16:34:09 +0100

    Update

--- components/addcar.php
+++ components/addcar.php
@@ -1,57 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/addcar.css">
+    <link rel="stylesheet" href="../css/addcar1.css">
     <title>ADMINISTRATOR</title>
 </head>
+
 <body>
-<button id="back"><a href="adminvehicle.php">HOME</a></button> 
-    
- <div class="main">
-        
+
+    <div class="main">
+        <div class="btn-home">
+            <h4 class="title-lo-icon">Home</h4>
+            <button class="back"><a href="./adminvehicle.php"><img src="../images/icon-home.png" width="30vw"
+                        alt=""></a></button>
+        </div>
         <div class="register">
-        <h2>Enter Details Of New Car</h2>
-        <form id="register"  action="upload.php" method="POST" enctype="multipart/form-data">    
-            <label>Car Name : </label>
-            <br>
-            <input type ="text" name="carname"
-            id="name" placeholder="Enter Car Name" required>
-            <br><br>
+            <h2>Enter Details Of New Car</h2>
+            <form id="register" action="upload.php" method="POST" enctype="multipart/form-data">
+                <label>Car Name : </label>
+                <br>
+                <input type="text" name="carname" id="name" placeholder="Enter Car Name" required>
+                <br><br>
 
-            <label>Fuel Type : </label>
-            <br>
-            <input type ="text" name="ftype"
-            id="name" placeholder="Enter Fuel Type" required>
-            <br><br>
+                <label>Fuel Type : </label>
+                <br>
+                <input type="text" name="ftype" id="name" placeholder="Enter Fuel Type" required>
+                <br><br>
 
-            <label>Capacity : </label>
-            <br>
-            <input type="number" name="capacity" min="1"
-            id="name" placeholder="Enter Capacity Of Car" required>
-            <br><br>
-            
-            <label>Price : </label>
-            <br>
-            <input type="number" name="price" min="1"
-            id="name" placeholder="Enter Price Of Car for One Day(in rupees)" required>
-            <br><br>
+                <label>Capacity : </label>
+                <br>
+                <input type="number" name="capacity" min="1" id="name" placeholder="Enter Capacity Of Car" required>
+                <br><br>
 
-            <label>Car Image : </label>
-            <br>
-            <input type="file" name="image" required>
-           <br><br>
+                <label>Price : </label>
+                <br>
+                <input type="number" name="price" min="1" id="name"
+                    placeholder="Enter Price Of Car for One Day(in rupees)" required>
+                <br><br>
 
-            <input type="submit" class="btnn"  value="ADD CAR" name="addcar">
-            
-        
-        
-        </input>
-            
-        </form>
-        </div> 
-    </div.main>
+                <label>Car Image : </label>
+                <br>
+                <input class="im-la" type="file" name="image" required>
+                <br><br>
+
+                <input type="submit" class="btnn" value="ADD CAR" name="addcar">
+
+
+
+                </input>
+
+            </form>
+        </div>
+        </div.main>
 </body>
+
 </html>
--- components/cardetails.php
+++ components/cardetails.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link  rel="stylesheet" href="../css/cardetails.css">
-
+    <link rel="stylesheet" href="../css/cardetails2.css">
     <title>Car Details</title>
-    
+
 </head>
 
 <body class="body">
 
-<style>
+    <style>
 
 
-</style>
+    </style>
 
 
-<?php 
+    <?php 
     require_once('connection.php');
         session_start();
 
@@ -33,60 +33,69 @@
         
     ?>
 
-</script>
-  <div class="cd">
-    <div class="main">
-        <div class="navbar">
-            <div class="icon">
-                <h2 class="logo"><img src="./images/logo.png" alt=""></h2>
-            </div>
-            <div class="menu">
-               
-                <ul>
-                    <li><a href="#">HOME</a></li>
-                    <li><a href="aboutus2.html">ABOUT</a></li>
-                    
-                    <li><a href="contactus.html">CONTACT</a></li>
-                    <li><a href="Feedbacks.php">FEEDBACK</a></li>
-                    <li><button class="nn"><a href="../index.php">LOGOUT</a></button></li>
-                    <!-- <li><img src="images/profile.png" class="circle" alt="Alps"></li> -->
-                    <li><p class="phello">HELLO! &nbsp;<a id="pname"><?php echo $rows['FNAME']." ".$rows['LNAME']?></a></p></li>
-                    <li><a id="stat" href="bookinstatus.php">BOOKING STATUS</a></li>
-                </ul>
-            </div>
-            
-            
-        </div>
-      <div><h1 class="overview">OUR CARS OVERVIEW</h1>
+    </script>
+    <div class="cd">
+        <div class="main">
+            <div class="navbar">
+                <div class="icon">
+                    <h2 class="logo"><img src="./images/logo.png" alt=""></h2>
+                </div>
+                <div class="menu">
 
-    <ul class="de">
-    <?php
+                    <ul>
+                        <li><a href="#">HOME</a></li>
+                        <li><a href="aboutus2.html">ABOUT</a></li>
+
+                        <li><a href="contactus.html">CONTACT</a></li>
+                        <li><a href="Feedbacks.php">FEEDBACK</a></li>
+                        <li><button class="nn"><a href="../index.php">LOGOUT</a></button></li>
+                        <!-- <li><img src="images/profile.png" class="circle" alt="Alps"></li> -->
+                        <li>
+                            <p class="phello">HELLO! &nbsp;<a
+                                    id="pname"><?php echo $rows['FNAME']." ".$rows['LNAME']?></a></p>
+                        </li>
+                        <li><a id="stat" href="bookinstatus.php">BOOKING STATUS</a></li>
+                    </ul>
+                </div>
+
+
+            </div>
+            <div>
+                <h1 class="overview">OUR CARS OVERVIEW</h1>
+                <div class="bbbox-coon">
+                    <ul class="de">
+                        <?php
         while($result= mysqli_fetch_array($cars))
         {
             
-    ?>    
-    
-    <li>
-    <form method="POST">
-    <div class="box">
-       <div class="imgBx">
-            <img src="../images/<?php echo $result['CAR_IMG']?>">
-        </div>
-        <div class="content">
-            <?php $res=$result['CAR_ID'];?>
-            <h1><?php echo $result['CAR_NAME']?></h1>
-            <h2>Fuel Type : <a><?php echo $result['FUEL_TYPE']?></a> </h2>
-            <h2>CAPACITY : <a><?php echo $result['CAPACITY']?></a> </h2>
-            <h2>Rent Per Day : <a><?php echo $result['PRICE']?> DH </a></h2>
-            <button type="submit"  name="booknow" class="utton" style="margin-top: 5px;"><a href="booking.php?id=<?php echo $res;?>">book</a></button>
-        </div>
-    </div></form>
-</li>
-    <?php
+    ?>
+
+                        <li>
+                            <form method="POST">
+                                <div class="box-con">
+                                    <div class="box">
+                                        <div class="imgBx">
+                                            <img class="box-im" src="../images/<?php echo $result['CAR_IMG']?>">
+                                        </div>
+                                        <div class="content">
+                                            <?php $res=$result['CAR_ID'];?>
+                                            <h1 class="car-tt"><?php echo $result['CAR_NAME']?></h1>
+                                            <h2>Fuel Type : <a><?php echo $result['FUEL_TYPE']?></a> </h2>
+                                            <h2>CAPACITY : <a><?php echo $result['CAPACITY']?></a> </h2>
+                                            <h2>Rent Per Day : <a><?php echo $result['PRICE']?> DH </a></h2>
+                                            <button type="submit" name="booknow" class="utton"
+                                                style="margin-top: 5px;"><a
+                                                    href="booking.php?id=<?php echo $res;?>">book</a></button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </li>
+                        <?php
         }
     
     ?>
-    <?php 
+                        <?php 
     require_once('connection.php');
         
 
@@ -103,13 +112,15 @@
     ?>
 
 
-         
-                
-           
-    </ul>
+
+
+
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-  </div>
 
 </body>
+
 </html>
