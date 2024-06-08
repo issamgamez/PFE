c61ff234e41e4f9e10584476751358f8011eb46e
YassineRazkaoui <razkaouiyassine9@gmail.com>
2024-06-08 01:17:40 +0100

    Update

--- components/adminusers.php
+++ components/adminusers.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/adminusers.css">
+    <!-- <link rel="stylesheet" href="../css/adminusers.css"> -->
+    <link rel="stylesheet" href="../css/adminusers1.css
+    ">
     <title>ADMINISTRATOR</title>
 </head>
+
 <body>
-<?php
+    <?php
 
 require_once('connection.php');
 $query="select *from users";
@@ -17,7 +21,7 @@ $num=mysqli_num_rows($queryy);
 
 
 ?>
-<div class="hai">
+    <div class="hai">
         <div class="navbar">
             <div class="icon">
                 <h2 class="logo">C R W</h2>
@@ -27,50 +31,52 @@ $num=mysqli_num_rows($queryy);
                     <li><a href="adminvehicle.php">VEHICLE MANAGEMENT</a></li>
                     <li><a href="adminusers.php">USERS</a></li>
                     <li><a href="admindash.php">FEEDBACKS</a></li>
-                    
                     <li><a href="adminbook.php">BOOKING REQUEST</a></li>
-                  <li> <button class="nn"><a href="../index.php">LOGOUT</a></button></li>
+                    <li> <button class="nn"><a href="../index.php">LOGOUT</a></button></li>
                 </ul>
             </div>
-            
-          
+
+
         </div>
         <div>
             <h1 class="header">USERS</h1>
             <div>
                 <div>
                     <table class="content-table">
-                <thead>
-                    <tr>
-                        <th>NAME</th> 
-                        <th>EMAIL</th>
-                        <th>LICENSE NUMBER</th>
-                        <th>PHONE NUMBER</th> 
-                        <th>GENDER</th> 
-                        <th>DELETE USERS</th>
-                    </tr>
-                </thead>
-                <tbody>
-                <?php
+                        <thead>
+                            <tr>
+                                <th class=" tableee">NAME</th>
+                                <th class=" tableee">EMAIL</th>
+                                <th class=" tableee">LICENSE NUMBER</th>
+                                <th class=" tableee">PHONE NUMBER</th>
+                                <th class=" tableee">GENDER</th>
+                                <th class=" tableee">DELETE USERS</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
                 
                 
                 while($res=mysqli_fetch_array($queryy)){
                  
                 
                 ?>
-                <tr  class="active-row">
-                    <td><?php echo $res['FNAME']."  ".$res['LNAME'];?></td>
-                    <td><?php echo $res['EMAIL'];?></td>
-                    <td><?php echo $res['LIC_NUM'];?></td>
-                    <td><?php echo $res['PHONE_NUMBER'];?></td>
-                    <td><?php echo $res['GENDER'];?></td>
-                    <td><button type="submit" class="but" name="approve"><a href="deleteuser.php?id=<?php echo $res['EMAIL']?>">DELETE USER</a></button></td>
-                </tr>
-               <?php } ?>
-                </tbody>
-                </table>
+                            <tr class="active-row">
+                                <td class="tbntt"><?php echo $res['FNAME']."  ".$res['LNAME'];?></td>
+                                <td class="tbntt"><?php echo $res['EMAIL'];?></td>
+                                <td class="tbntt"><?php echo $res['LIC_NUM'];?></td>
+                                <td class="tbntt"><?php echo $res['PHONE_NUMBER'];?></td>
+                                <td class="tbntt"><?php echo $res['GENDER'];?></td>
+                                <td class="tbntt"><button type="submit" class="but" name="approve"><a
+                                            href="deleteuser.php?id=<?php echo $res['EMAIL']?>">DELETE USER</a></button>
+                                </td>
+                            </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
 </body>
+
 </html>
